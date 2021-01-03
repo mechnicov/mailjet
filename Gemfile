be63066c3b43ca36954f2799be8d784efb0c5d33
Edem Topuzov <keltikan@gmail.com>
2021-01-03 18:06:40 +0300

    Initial commit

---
+++ Gemfile
@@ -0,0 +1,9 @@
+source "https://rubygems.org"
+git_source(:github) { |repo| "https://github.com/#{repo}.git" }
+
+ruby "2.7.1"
+
+gem "rails", "~> 6.0.3", ">= 6.0.3.1"
+gem "sqlite3", "~> 1.4"
+gem "puma", "~> 4.1"
+gem "webpacker", "~> 4.0"
---
+++ config/application.rb
@@ -0,0 +1,18 @@
+require_relative "boot"
+
+require "rails"
+require "active_model/railtie"
+require "active_job/railtie"
+require "active_record/railtie"
+require "action_controller/railtie"
+require "action_mailer/railtie"
+require "action_view/railtie"
+
+Bundler.require(*Rails.groups)
+
+module Mailjet
+  class Application < Rails::Application
+    config.load_defaults 6.0
+    config.generators.system_tests = nil
+  end
+end
